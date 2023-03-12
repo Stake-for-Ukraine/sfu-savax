a6e6c0121bf8473c94af351bbc6c4711535eb4b2
Oleksiy Novykov <services@novykov.com>
2023-03-12 17:57:14 +0100

    Natspec in strategies

--- src/strategies/DistributeStrategy1.sol
+++ src/strategies/DistributeStrategy1.sol
@@ -4,25 +4,50 @@ pragma solidity ^0.8.0;
 import '../interfaces/IdistStrategy.sol';
 import 'openzeppelin-contracts/token/ERC20/IERC20.sol';
 
+/**
+ * @title DistributeStrategy1
+ * @notice DistributeStrategy1 contract is an execution contract that is used by Harvest Manager to distribute funds to beneficiaries. 
+ */
 contract DistributeStrategy1 is IdistStrategy {
-    address[] public beneficiaries;
-    uint8[] public percentages;
-    address public owner;
 
+    /// @notice Array of addresses of beneficiaries who recieve funding
+    address[] public beneficiaries;
+
+    /// @notice Array of percentages of how available funds should be splitted between beneficiaries
+    /// @dev Percentages must be between 0 and 100. Sum of percentages must be equal to 100.
+
+    uint8[] public percentages;
+    /// @notice Address of the owner of the contract
+    address public manager;
+
+    /// @notice Event emitted when funds are distributed to beneficiaries
+    /// @param _distributionToken Address of the token that is distributed
+    /// @param _amount Amount of the token that is distributed
     event Distribute(address indexed _distributionToken, uint256 _amount);
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can call this function");
+    /// @notice modifier that is used to restrict access to the function only to the manager;
+    modifier onlyManager() {
+
+        require(msg.sender == manager, "Only manager can call this function.");
         _;
+
     }
 
-    function initialize(address[] memory _beneficiaries, uint8[] memory _percentages) external {
+    /// @notice Contract constructor.
+    /// @param _beneficiaries Array of addresses of beneficiaries who recieve funding
+    /// @param _percentages Array of percentages of how available funds should be splitted between beneficiaries
+    constructor (address[] memory _beneficiaries, uint8[] memory _percentages) {
+
         require(_beneficiaries.length == _percentages.length, "Lengths must be equal");
         beneficiaries = _beneficiaries;
         percentages = _percentages;
+        
     }
 
-    function distribute(address _distributionToken) external onlyOwner {
+    /// @notice Function that is called by Harvest Manager to distribute funds to beneficiaries.
+    /// @param _distributionToken Address of the ERC-20 token that is distributed
+    function distribute(address _distributionToken) external onlyManager {
+
         require(beneficiaries.length == percentages.length, "Lengths must be equal");
         for(uint i = 0; i < percentages.length; i++) {
             require(percentages[i] >= 0 && percentages[i] <= 100, "Value must be between 0 and 100");
@@ -38,6 +63,7 @@ contract DistributeStrategy1 is IdistStrategy {
                 uint256 _amount = _fundsToDistribute * percentages[i] / 100;
                 IERC20(_distributionToken).transfer(beneficiaries[i], _amount);
                 emit Distribute(_distributionToken, _amount);
+                
             }
         }
     }
