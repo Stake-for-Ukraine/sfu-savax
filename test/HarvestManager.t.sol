e909cdc53a689485a3f64521f7d4b69f8885183e
Oleksiy Novykov <services@novykov.com>
2023-05-29 16:39:53 +0200

    Tests for manager and strategy

--- test/HarvestManager.t.sol
+++ test/HarvestManager.t.sol
@@ -3,29 +3,42 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "../src/HarvestManager.sol";
-import "./mocks/MockLSDVault.sol";
-import "./mocks/MockERC20.sol";
+import "../src/interfaces/IStakedAvax.sol";
 import {DistributeToTreasury} from "../src/strategies/TempStrat.sol";
 
 contract HarvestManagerTest is Test {
     HarvestManager public harvestManager;
-    MockLSDVault public mockLSDVault;
-    MockERC20 public USDC;
     DistributeToTreasury public distributeToTreasury;
+    IStakedAvax public sAVAXcontract;
 
-    address[] beneficiaries = new address[](2);
-    uint8[] percentages = new uint8[](2);
+    address payable public sAVAXaddress;
+    address payable public treasuryAddress;
 
     function setUp() public {
+        uint256 forkId = vm.createFork("mainnet");
+        vm.selectFork(forkId);
+
+
+        sAVAXaddress = payable(0x2b2C81e08f1Af8835a78Bb2A90AE924ACE0eA4bE);
+        treasuryAddress = payable(0xf6Df9dcF3e3D07437e3e583d1cC41d9C8FB53Ae8);
+        sAVAXcontract = IStakedAvax(sAVAXaddress);
+
         harvestManager = new HarvestManager();
-        mockLSDVault = new MockLSDVault();
-        USDC = new MockERC20();
+        distributeToTreasury = new DistributeToTreasury(address(harvestManager), sAVAXaddress, treasuryAddress);
     }
 
     function testDistribution() public {
-        distributeToTreasury = new DistributeToTreasury(address(harvestManager), address(USDC), address (mockLSDVault));
         harvestManager.updateDistributeStrategy(address(distributeToTreasury));
-        mockLSDVault.mint(address(harvestManager), 100);
-        
+        console.log("my AVAX balance:", address(this).balance);
+        sAVAXcontract.submit{value: 100 ether}();
+        console.log("my AVAX balance:", address(this).balance);
+        console.log("my sAVAX balance:", sAVAXcontract.balanceOf(address(this)));
+        sAVAXcontract.transfer(address(harvestManager), 100);
+        harvestManager.distribute(sAVAXaddress);
+
+        assertEq(sAVAXcontract.balanceOf(address(harvestManager)), 0);
+        assertEq(sAVAXcontract.balanceOf(address(distributeToTreasury)), 0);
+        require (sAVAXcontract.balanceOf(treasuryAddress) > 0);
     }
+
 }
